d655e2bb37238f66f6a86037359620e3cb0509e0
Jonathan Ibrahim <nji3000@gmail.com>
2021-06-23 14:54:29 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ecommerce Store
+ An extensive project building out an ecommerce store using NodeJS
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieSession({
+  keys: ['lkafjdlkfjkasjlgkjdf32082']
+}));
+app.use(authRouter);
+app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(cartsRouter);
+
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
